732a66444bac639f7a8f34179cf6d68dfcca087c
helplynch <108618400+helplynch@users.noreply.github.com>
2024-08-05 13:28:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,14 +8,10 @@
 // @grant        none
 // ==/UserScript==
 (function() {
-    var newUserAgent = "Mozilla/5.0 (Linux; Android 13; SM-S901B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36";
-    var newPlatform = "Linux armv8l";
-    var newVendor = "SilentSpoofer Inc."; 
-    var newAppVersion = "5.0 (Linux; Android 13; SM-S901B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36";
-    var newAppName = "Netscape";
+    var spoofedValue = "N/A";
 
-    function overrideProperty(property, value) {
-        Object.defineProperty(navigator, property, {
+    function overrideProperty(obj, property, value) {
+        Object.defineProperty(obj, property, {
             get: function() {
                 return value;
             },
@@ -23,26 +19,46 @@
         });
     }
 
-    overrideProperty('userAgent', newUserAgent);
-    overrideProperty('platform', newPlatform);
-    overrideProperty('vendor', newVendor);
-    overrideProperty('appVersion', newAppVersion);
-    overrideProperty('appName', newAppName);
+    var propertiesToSpoof = [
+        'userAgent',
+        'platform',
+        'vendor',
+        'appVersion',
+        'appName',
+        'product',
+        'productSub',
+        'vendorSub',
+        'oscpu',
+        'language',
+        'languages',
+        'hardwareConcurrency',
+        'deviceMemory'
+    ];
 
-    var originalGetUserAgent = navigator.userAgent;
-    navigator.__defineGetter__('userAgent', function(){
-        return newUserAgent;
+    propertiesToSpoof.forEach(function(property) {
+        overrideProperty(navigator, property, spoofedValue);
     });
 
-    var originalGetAppVersion = navigator.appVersion;
-    navigator.__defineGetter__('appVersion', function(){
-        return newAppVersion;
-    });
+    var methodsToSpoof = [
+        'getBattery',
+        'getGamepads',
+        'getVRDisplays',
+        'getUserMedia',
+        'javaEnabled',
+        'sendBeacon',
+        'vibrate'
+    ];
 
+    methodsToSpoof.forEach(function(method) {
+        navigator[method] = function() {
+            return spoofedValue;
+        };
+    });
+    
     if (window.XMLHttpRequest) {
         var originalOpen = XMLHttpRequest.prototype.open;
         XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-            this.setRequestHeader("User-Agent", newUserAgent);
+            this.setRequestHeader("User-Agent", spoofedValue);
             originalOpen.apply(this, arguments);
         };
     }
@@ -51,18 +67,38 @@
         var originalFetch = window.fetch;
         window.fetch = function(input, init) {
             if (init && init.headers) {
-                init.headers['User-Agent'] = newUserAgent;
+                init.headers['User-Agent'] = spoofedValue;
             } else if (typeof input === 'string') {
                 init = {
                     headers: {
-                        'User-Agent': newUserAgent
+                        'User-Agent': spoofedValue
                     }
                 };
             } else if (input instanceof Request) {
-                input.headers.set('User-Agent', newUserAgent);
+                input.headers.set('User-Agent', spoofedValue);
             }
             return originalFetch(input, init);
         };
     }
-})();
 
+    overrideProperty(document, 'referrer', spoofedValue);
+    overrideProperty(document, 'cookie', spoofedValue);
+    overrideProperty(document, 'domain', spoofedValue);
+
+    console.log("User Agent spoofed to: " + navigator.userAgent);
+    console.log("Platform spoofed to: " + navigator.platform);
+    console.log("Vendor spoofed to: " + navigator.vendor);
+    console.log("App Version spoofed to: " + navigator.appVersion);
+    console.log("App Name spoofed to: " + navigator.appName);
+    console.log("Product spoofed to: " + navigator.product);
+    console.log("ProductSub spoofed to: " + navigator.productSub);
+    console.log("VendorSub spoofed to: " + navigator.vendorSub);
+    console.log("OSCPU spoofed to: " + navigator.oscpu);
+    console.log("Language spoofed to: " + navigator.language);
+    console.log("Languages spoofed to: " + navigator.languages);
+    console.log("HardwareConcurrency spoofed to: " + navigator.hardwareConcurrency);
+    console.log("DeviceMemory spoofed to: " + navigator.deviceMemory);
+    console.log("Document referrer spoofed to: " + document.referrer);
+    console.log("Document cookie spoofed to: " + document.cookie);
+    console.log("Document domain spoofed to: " + document.domain);
+})();
